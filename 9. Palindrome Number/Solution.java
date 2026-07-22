d474fb448c7b4396caedef08ec15a2578ce84dd1
saravana5632 <oatktg.saravana5632@gmail.com>
2026-07-22 23:27:05 +0530

    9. Palindrome Number Initial Commit

---
+++ 9. Palindrome Number/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0)
+            return false; 
+        int num=x;
+        int res=0;
+        while(x>0)
+        {
+            res=res*10 + (x%10);
+            x/=10;
+        }
+    return num==res;
+    }
+
+}
