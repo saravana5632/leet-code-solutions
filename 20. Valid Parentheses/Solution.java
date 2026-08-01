3e7ccf7e38bd444a429d7db90c38e6d87fee9bd3
saravana5632 <oatktg.saravana5632@gmail.com>
2026-08-01 22:33:25 +0530

    Added Comments for better code clarity

--- 20. Valid Parentheses/Solution.java
+++ 20. Valid Parentheses/Solution.java
@@ -4,22 +4,26 @@ public class Solution {
          Stack <Character> res = new Stack<>();
          for(int i =0 ; i<s.length() ; i++){
             if(s.charAt(i)=='('  || s.charAt(i)=='{' || s.charAt(i)=='[')
-                res.push(s.charAt(i));    
+                res.push(s.charAt(i));   // push elements to stack
+
             else if (s.charAt(i)==')' || s.charAt(i)=='}' || s.charAt(i)==']')
             {
-                if(res.isEmpty())
+                if(res.isEmpty()) //Checks for Open Parenthesis return false if no open parenthesis
                     return false;
-                char temp= s.charAt(i);
-                if (res.peek()=='(' && temp==')')
+                
+                char temp= s.charAt(i); //stores current Parenthesis
+                char peek1=res.peek(); // stores top of the stack
+                
+                if (peek1=='(' && temp==')')
                     res.pop();
-                else if (res.peek()=='{' && temp=='}')
+                else if (peek1=='{' && temp=='}')
                     res.pop();
-                else if (res.peek()=='[' && temp==']')
+                else if (peek1=='[' && temp==']')
                     res.pop();
                 else 
-                 return false;
+                    return false;
             }
             }
-         return res.isEmpty();
+         return res.isEmpty(); //returns true if stack is empty, else false
          }
         }
